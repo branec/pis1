d1723da5c75cc155aae11bb7bee8b2223a5c90dc
abegan <abegan00@fesb.hr>
2016-03-07 08:52:50 +0100

    Update Program.cs

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -11,6 +11,8 @@ namespace ConsoleApplication1
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World");
+            //men's čini da nevalja
         }
     }
 }
+//nezz nevalja ovo baš
